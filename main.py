cc79652f02eead769ce280abe6ef5d5f8b12f80e
Mannety_ Dev <>
2021-05-13 12:01:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+
+# name = "Mannety"
+# print(name)
+
+#================
+# a = b = c = 'cat'
+# print(a)
+# print(b)
+# print(c)
+
+#===============
+# colour = 'Red'
+# colour = 'Blue'
+
+# print(colour)
+
+#================
+# var = "Hello world"
+# print(type(var))
+
+#================
+# var = 23
+# print(type(var))
+
+# score = 400 
+# print(id(score))
+
+# score = 400
+# pb = score
+# print(id(score))
+# print(id(pb))
+
+# score = 100
+# pb = score
+
+# print(type(score))
+# print(type(pb))
+# print(score)
+# print(pb)
+
+pb = 20 
+score = 100
+score = 'Completed'
+
+print(type(score))
+print(type(pb))
+print(score)
+print(pb)
